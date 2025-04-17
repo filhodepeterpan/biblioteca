07f505df08a96448b084a34ad36445c76af46272
Douglas Soares <douglassoares.cinema@gmail.com>
2025-04-17 03:04:52 -0300

    montando layout e lógica da página LIVROS

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,24 @@
+const campoBuscar = document.getElementById("inputBusca");
+const botaoBuscar = document.getElementById("buscar");
+
+campoBuscar.addEventListener("keypress", function(event){
+  if (event.key === "Enter"){
+    buscaLivros();
+  }
+});
+
 function buscaLivros() {
-    const termo = document.getElementById("searchInput").value;
+    const termo = document.getElementById("inputBusca").value;
     const categoria = document.getElementById("filtroCategoria").value;
+    const nomeDaCategoria = document.getElementById("nome-da-categoria");
+
     let url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(termo)}`;
   
     if (categoria) {
       url += `+subject:${encodeURIComponent(categoria)}`;
     }
+
+    categoria? nomeDaCategoria.innerHTML = `Categoria: ${categoria}`: nomeDaCategoria.innerHTML = `Todos`;
   
     fetch(url)
       .then(res => res.json())
@@ -22,15 +35,20 @@ function buscaLivros() {
           const info = livro.volumeInfo;
           const capa = info.imageLinks?.thumbnail || "";
           const titulo = info.title || "Sem título";
+          const generos = info.categories?.[0] || "Desconhecido";
           const autores = info.authors?.join(", ") || "Autor desconhecido";
   
-          container.innerHTML += `
-            <section>
-              <img src="${capa}" alt="Capa" style="height:150px;">
-              <h3>${titulo}</h3>
-              <p><strong>Autor(es):</strong> ${autores}</p>
-            </section>
-          `;
+          container.innerHTML += 
+            `
+              <section>
+                <img src="${capa}" alt="Capa de ${titulo}" id="livro-capa">
+                <div id="livro-info">
+                  <b>${titulo}</b>
+                  <p><strong>Autor(es):</strong> ${autores}</p>
+                  <p><strong>Gênero(s):</strong> ${generos}</p>
+                </div>
+              </section>
+            `;
         });
       })
       .catch(err => {
